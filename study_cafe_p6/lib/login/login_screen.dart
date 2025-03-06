41bd3c0c456e909717659dd7ff9c9b9a1829c75b
DeokWon Kim <khjs7878@naver.com>
2025-03-07 00:07:44 +0900

    .gitignore 파일 업데이트 및 로그인 화면에서 Firebase 인증 관련 코드 제거

--- study_cafe_p6/lib/login/login_screen.dart
+++ study_cafe_p6/lib/login/login_screen.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:study_cafe_p6/Screen/reserve_screen.dart';
