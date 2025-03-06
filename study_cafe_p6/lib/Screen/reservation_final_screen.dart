8913d1f45e3fe1eafd63f95073bed67d29ab75f0
Yung Hak Lee <celebi3@naver.com>
2025-03-06 22:47:23 +0900

    Feat: ReservationFinalScreen 레이아웃 변경
    사용자 예약정보 박스 레이아웃 변경

--- study_cafe_p6/lib/Screen/reservation_final_screen.dart
+++ study_cafe_p6/lib/Screen/reservation_final_screen.dart
@@ -19,32 +19,42 @@ class ReservationFinalScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('예약하기'), backgroundColor: Color(0xfff8f2de)),
+      appBar: AppBar(
+        title: Text('사용자 예약정보'),
+        backgroundColor: Color(0xfff8f2de),
+      ),
       body: Center(
         child: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(height: 20),
-              Text('사용자 예약정보', style: TextStyle(fontSize: 20)),
-              SizedBox(height: 20),
+              SizedBox(height: 50),
               Container(
-                width: 250,
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.black),
                   borderRadius: BorderRadius.circular(12),
                 ),
                 child: Padding(
                   padding: const EdgeInsets.symmetric(vertical: 20.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        '선택한 이용권: $selectedPlan\n결제 금액: ${formatAmount(selectedPrice)}',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text('금액', style: TextStyle(fontSize: 20)),
-                      Text('좌석 정보', style: TextStyle(fontSize: 20)),
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          '선택한 이용권: $selectedPlan',
+                          style: TextStyle(fontSize: 25),
+                          softWrap: true,
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          '결제 금액: ${formatAmount(selectedPrice)}',
+                          style: TextStyle(fontSize: 25),
+                          softWrap: true,
+                        ),
+                        SizedBox(height: 20),
+                        Text('좌석 정보', style: TextStyle(fontSize: 25)),
+                      ],
+                    ),
                   ),
                 ),
               ),
