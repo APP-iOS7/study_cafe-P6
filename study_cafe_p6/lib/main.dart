e10fb48eb2d35d333117742def48f25aecfef156
DeokWon Kim <khjs7878@naver.com>
2025-03-07 18:05:10 +0900

    Feat: main.dart에서 불필요한 import 제거 및 코드 정리

--- study_cafe_p6/lib/main.dart
+++ study_cafe_p6/lib/main.dart
@@ -2,14 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:study_cafe_p6/Screen/myInfo_screen.dart';
-import 'package:study_cafe_p6/Screen/reservationhistory_screen.dart';
 import 'package:study_cafe_p6/Screen/tabbar_screen.dart';
-import 'package:study_cafe_p6/Screen/reservation_screen.dart';
 import 'package:get/route_manager.dart';
 import 'package:study_cafe_p6/firebase_options.dart';
 import 'package:study_cafe_p6/login/login_screen.dart';
-import 'package:study_cafe_p6/view/seat_page_view.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
